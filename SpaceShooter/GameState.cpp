16df9c1fc9cb54fea3182529939c28cbcd87e033
Sebastian Szynk <s.szynk98@gmail.com>
2020-11-28 00:57:03 +0100

    Added background animation

--- SpaceShooter/GameState.cpp
+++ SpaceShooter/GameState.cpp
@@ -1,13 +1,46 @@
 #include "GameState.h"
 
+
 GameState::GameState(sf::RenderWindow* window, std::stack<State*>* states) : State(window, states) {
-
+	initBackground();
+	backgroundTime = 0.f;
+	bgIndex = 0;
 }
 
-void GameState::update(const float& dt)
-{
+
+void GameState::initBackground() {
+	std::string path = "../Resources/background/space";
+
+	sf::Vector2f scale = (sf::Vector2f)window->getSize() / 200.f;
+
+	for (int i = 0; i < 6; i++) {
+		backgroundTextures[i].loadFromFile(path + std::to_string(i) + ".png");
+		backgroundSprites[i].setTexture(backgroundTextures[i]);
+		backgroundSprites[i].setScale(scale);
+	}
 }
 
-void GameState::render()
-{
+
+void GameState::update(const float& dt) {
+
+	updateBackground(dt);
+
+	updateMousePosition();
+
+	updateSFMLEvents();
+}
+
+void GameState::updateBackground(const float& dt) {
+	backgroundTime += dt;
+	std::cout << 1 / dt << "\n";
+	//change every second
+	if (backgroundTime > 1.f) {
+
+		bgIndex >= 5 ? bgIndex = 0 : bgIndex++;
+		backgroundTime = 0.f;
+	}
+}
+
+void GameState::render() {
+	window->draw(backgroundSprites[bgIndex]);
 }
--- SpaceShooter/GameState.h
+++ SpaceShooter/GameState.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "State.h"
+#include <iostream>
 class GameState : public State {
+	sf::Texture backgroundTextures[6];
+	sf::Sprite backgroundSprites[6];
+
+	float backgroundTime;
+	int bgIndex;
+
+
+	void initBackground();
+
+	void updateBackground(const float& dt);
 
 public:
 	GameState(sf::RenderWindow* window, std::stack<State*>* states);
