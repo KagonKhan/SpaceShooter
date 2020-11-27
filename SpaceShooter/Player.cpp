69adc36dd33cb5e9a4fd7d6d2347c28f5ac0d595
Sebastian Szynk <s.szynk98@gmail.com>
2020-11-28 00:51:55 +0100

    Added Player movement

--- SpaceShooter/Player.cpp
+++ SpaceShooter/Player.cpp
@@ -1,1 +1,44 @@
 #include "Player.h"
+
+Player::Player(std::string filePath) {
+	playerTexture.loadFromFile(filePath);
+	playerSprite.setTexture(playerTexture);
+	playerSprite.setPosition(550.f, 600.f);
+
+	movementSpeed = 150.f;
+
+}
+
+void Player::updateMovement(const float& dt) {
+	movement *= 0.93f;
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		movement.x -= movementSpeed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		movement.x += movementSpeed;
+
+	if (playerSprite.getPosition().x < 150 && movement.x < 0 || playerSprite.getPosition().x > 730 && movement.x > 0)
+		movement *= 0.85f;
+
+	if (playerSprite.getPosition().x < 100 && movement.x < 0 || playerSprite.getPosition().x > 780 && movement.x > 0)
+		movement *= 0.65f;
+
+	if (playerSprite.getPosition().x < 50 && movement.x < 0 || playerSprite.getPosition().x > 890 && movement.x > 0)
+		movement *= 0.45f;
+
+	if (playerSprite.getPosition().x < 20 && movement.x < 0 || playerSprite.getPosition().x > 920 && movement.x > 0)
+		movement *= 0.0f;
+
+	playerSprite.move(movement * dt);
+}
+
+
+void Player::update(const float& dt) {
+	updateMovement(dt);
+
+}
+
+void Player::render(sf::RenderWindow* window) {
+	window->draw(playerSprite);
+}
--- SpaceShooter/Player.h
+++ SpaceShooter/Player.h
@@ -1,5 +1,28 @@
 #pragma once
-class Player
-{
+#include <iostream>
+#include <vector>
+
+#include "SFML/Graphics.hpp"
+
+class Player {
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+
+	float movementSpeed, attackSpeed, attackTime, projectileSpeed;
+	sf::Vector2f movement;
+
+
+
+
+
+	void updateMovement(const float& dt);
+
+
+public:
+	Player(std::string filePath);
+	~Player() {};
+
+	void update(const float& dt);
+	void render(sf::RenderWindow* window);
 };
 
