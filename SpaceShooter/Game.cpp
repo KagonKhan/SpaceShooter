9129dd5dafeee6bc2fa1e93ad395ced5a53d5d89
Sebastian Szynk <s.szynk98@gmail.com>
2020-11-28 00:48:53 +0100

    Added support for states

--- SpaceShooter/Game.cpp
+++ SpaceShooter/Game.cpp
@@ -1,5 +1,15 @@
 #include "Game.h"
 
+Game::Game() {
+	initWindow();
+	initStates();
+}
+
+Game::~Game() {
+	window->~RenderWindow();
+	delete window;
+}
+
 void Game::initWindow() {
 	window = new sf::RenderWindow(sf::VideoMode(1920, 1024), "Space Shooter");
 	window->setFramerateLimit(120);
@@ -7,25 +17,35 @@ void Game::initWindow() {
 	window->setVerticalSyncEnabled(true);
 }
 
-void Game::update() {
+void Game::initStates() {
+	states.push(new GameState(window, &states));
+}
 
+void Game::update() {
+	updateDeltaTime();
+
+
+	if (!states.empty())
+		states.top()->update(dt);
+	else
+		throw std::runtime_error("State stack empty!\n");
+}
+
+void Game::updateDeltaTime() {
+	dt = deltaTimeClock.restart().asSeconds();
 }
  
 void Game::render() {
 	window->clear();
 
+	if (!states.empty())
+		states.top()->render();
+	else
+		throw std::runtime_error("State stack empty!\n");
 
 	window->display();
 }
 
-Game::Game() {
-	initWindow();
-}
-
-Game::~Game() {
-	window->~RenderWindow();
-	delete window;
-}
 
 void Game::run() {
 	while (window->isOpen()) {
--- SpaceShooter/Game.h
+++ SpaceShooter/Game.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include "GameState.h"
+
 
 class Game {
 	sf::RenderWindow* window;
+	std::stack<State*> states;
+
+	sf::Clock deltaTimeClock;
+	float dt;
 
 	void initWindow();
+	void initStates();
 
 	void update();
+	void updateDeltaTime();
 
 	void render();
 
