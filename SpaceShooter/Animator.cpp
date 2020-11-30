f7b243bea3c45f99d0f8960590407df63a257815
Sebastian Szynk <s.szynk98@gmail.com>
2020-11-30 16:29:27 +0100

    Finishing up basic functionality

--- SpaceShooter/Animator.cpp
+++ SpaceShooter/Animator.cpp
@@ -1,1 +1,72 @@
 #include "Animator.h"
+
+Animator::Animator(sf::Sprite& sprite)
+	: m_Sprite(sprite), m_CurrentTime(), m_CurrentAnimation(nullptr){}
+
+Animator::Animation& Animator::CreateAnimation(const std::string& name, const std::string& filepath, const std::string& textureName, const sf::Time& duration, bool loop) {
+	m_Animations.push_back(Animator::Animation(name, filepath, textureName, duration, loop));
+
+	// if we have no other animations use that as current animation
+	if (m_CurrentAnimation == nullptr)
+		SwitchAnimation(&m_Animations.back());
+
+
+	return m_Animations.back();
+}
+
+void Animator::Update(const float& dt) {
+	if (!m_CurrentAnimation)
+		return;
+
+	m_CurrentTime += dt;
+
+	//Current animation frame
+	float scaledTime = (m_CurrentTime / m_CurrentAnimation->m_Duration.asSeconds());
+	int numberOfFrames = m_CurrentAnimation->m_Frames.size();
+
+	//not looping for now 
+	int currentFrame = static_cast<int> (scaledTime * numberOfFrames);
+
+	//if looping
+	if (m_CurrentAnimation->m_Looping)
+		currentFrame %= numberOfFrames;
+	else if (currentFrame >= numberOfFrames)
+		currentFrame = numberOfFrames - 1;
+
+
+	m_Sprite.setTextureRect(m_CurrentAnimation->m_Frames[currentFrame]);
+}
+
+bool Animator::SwitchAnimation(const std::string& name) {
+
+	auto animation = FindAnimation(name);
+	if (animation) {
+		SwitchAnimation(animation);
+		return true;
+	}
+	return false;
+}
+
+std::string Animator::GetCurrentAnimationName() const {
+	if (m_CurrentAnimation)
+		return m_CurrentAnimation->m_Name;
+	//no current animation playing
+	return "";
+}
+
+void Animator::SwitchAnimation(Animator::Animation* animation) {
+
+	if (animation)
+		m_Sprite.setTexture(AssetManager::GetTexture(animation->m_TextureName, animation->m_Filepath));
+
+	m_CurrentAnimation = animation;
+	m_CurrentTime = 0; // reset the time;
+}
+
+Animator::Animation* Animator::findAnimation(const std::string& name) {
+	for (auto it = m_Animations.begin(); it != m_Animations.end(); it++)
+		if (it->m_Name == name)
+			return &*it;
+
+	return nullptr;
+}
--- SpaceShooter/Animator.h
+++ SpaceShooter/Animator.h
@@ -1,5 +1,69 @@
 #pragma once
-class Animator
-{
+#include "SFML/Graphics.hpp"
+#include "AssetManager.h"
+#include <list>
+class Animator {
+
+	struct Animation;
+
+	//returns animation with the passed name, or nullptr if not found
+	Animator::Animation* FindAnimation(const std::string& name);
+
+	void SwitchAnimation(Animator::Animation* animation);
+
+	sf::Sprite& m_Sprite;
+	float m_CurrentTime;
+	std::list<Animator::Animation> m_Animations;
+	Animator::Animation* m_CurrentAnimation;
+
+
+	void SwitchAnimation(Animator::Animation* animation);
+
+	Animator::Animation* findAnimation(const std::string& name);
+
+public:
+	struct Animation {
+		std::string m_Name;
+		std::string m_Filepath;
+		std::string m_TextureName;
+		std::vector<sf::IntRect> m_Frames;
+		sf::Time m_Duration;
+		bool m_Looping;
+
+
+		Animation(const std::string& name, const std::string& filepath, const std::string& textureName,
+			const sf::Time& duration, bool looping)
+			: m_Name(name), m_Filepath(filepath), m_TextureName(textureName),
+			m_Duration(duration), m_Looping(looping) {}
+
+		//Adding horizontal name
+		void AddFrames(const sf::Vector2i& startFrom,
+			const sf::Vector2i& frameSize, unsigned int frames) {
+
+			sf::Vector2i current = startFrom;
+
+			for (unsigned int i = 0; i < frames; i++) {
+				m_Frames.push_back(sf::IntRect(current.x, current.y, frameSize.x, frameSize.y));
+
+				current.x += frameSize.x;
+			}
+		}
+	};
+
+
+	Animator(sf::Sprite& sprite);
+
+	Animator::Animation& CreateAnimation(const std::string& name, const std::string& filepath,
+		const std::string& textureName, const sf::Time& duration, bool loop = false);
+
+
+
+	void Update(const float& dt);
+
+	bool SwitchAnimation(const std::string& name);
+
+
+	std::string GetCurrentAnimationName() const ;	
+
 };
 
